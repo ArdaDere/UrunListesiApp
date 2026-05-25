fe9a9ffce09b961383f6743b8159c2cb395616c1
Arda Dere <ardadere114@gmail.com>
2026-05-26 00:35:29 +0300

    fix: partial class wrapper ve namespace duzeltildi

--- ConsoleApp4/Form1.Designer.cs
+++ ConsoleApp4/Form1.Designer.cs
@@ -1,41 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp4
+namespace UrunGosterApp
 {
-    private System.Windows.Forms.Button btnGoster;
-    private System.Windows.Forms.ListBox listBoxUrunler;
-
-    private void InitializeComponent()
+    partial class Form1
     {
-        this.btnGoster = new System.Windows.Forms.Button();
-        this.listBoxUrunler = new System.Windows.Forms.ListBox();
-        this.SuspendLayout();
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.ListBox listBoxUrunler;
 
-        // btnGoster
-        this.btnGoster.Location = new System.Drawing.Point(30, 30);
-        this.btnGoster.Name = "btnGoster";
-        this.btnGoster.Size = new System.Drawing.Size(150, 30);
-        this.btnGoster.Text = "Ürünleri Göster";
-        this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.listBoxUrunler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
 
-        // listBoxUrunler
-        this.listBoxUrunler.FormattingEnabled = true;
-        this.listBoxUrunler.ItemHeight = 15;
-        this.listBoxUrunler.Location = new System.Drawing.Point(30, 80);
-        this.listBoxUrunler.Name = "listBoxUrunler";
-        this.listBoxUrunler.Size = new System.Drawing.Size(300, 150);
+            this.btnGoster.Location = new System.Drawing.Point(30, 30);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(150, 30);
+            this.btnGoster.Text = "Urunleri Goster";
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
 
-        // Form1
-        this.ClientSize = new System.Drawing.Size(400, 300);
-        this.Controls.Add(this.btnGoster);
-        this.Controls.Add(this.listBoxUrunler);
-        this.Name = "Form1";
-        this.Text = "Ürün Listesi";
-        this.ResumeLayout(false);
+            this.listBoxUrunler.FormattingEnabled = true;
+            this.listBoxUrunler.ItemHeight = 15;
+            this.listBoxUrunler.Location = new System.Drawing.Point(30, 80);
+            this.listBoxUrunler.Name = "listBoxUrunler";
+            this.listBoxUrunler.Size = new System.Drawing.Size(300, 150);
+
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.listBoxUrunler);
+            this.Name = "Form1";
+            this.Text = "Urun Listesi";
+            this.ResumeLayout(false);
+        }
     }
-
 }
